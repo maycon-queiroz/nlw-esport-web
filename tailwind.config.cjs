1d252f428616b230a3bbe740bd8aafc9dc4fb26e
Maycon Queiroz <fmaycon@gmail.com>
2022-09-14 23:34:30 -0300

    style: add main css, font family and custom backgrounds

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,16 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    extend: {},
+     fontFamily: {
+      'sans': ['Inter', 'sans-serif'],      
+    },
+    extend: {
+      backgroundImage:{
+        galaxy: "url('./background-galaxy.png')",
+        'nlw-gradient': "linear-gradient(89.86deg, #9572FC 27.08%, #43e7ad 33.94%, #e1d55d 40.57%)",
+        'game-gradient': "linear-gradient(188deg, rgba(0,0,0,0) 0%,  rgba(0,0,0,0.8) 47.08%)"
+      }
+    },
   },
   plugins: [],
 }
