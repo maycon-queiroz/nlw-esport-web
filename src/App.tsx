eab72a5dcc366ffa7d61f57e5081ff41f5df5379
Maycon Queiroz <fmaycon@gmail.com>
2022-09-17 18:29:43 -0300

    feat(states): add useStates and useEffect update states and list of the banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 
+import { useState, useEffect } from 'react';
+
 import './styles/main.css';
 import LogoImage from './assets/logo-nlw-esports.svg'
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
 
+interface Game {
+  bannerUrl: string;
+  id: string;
+  title: string;
+  _count: {
+    ads: number;
+  }
+}
+
 function App() {
+  const [games, setGame] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => setGame(data))
+  }, [])
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center">
       <img className="mt-10" src={LogoImage} alt="Logo com o texto Esports NLW" />
@@ -13,12 +32,16 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-10">
-        <GameBanner title='Word WarCraff' adsCount={4} bannerUrl="gamer-1.png" />
-        <GameBanner title='League of Legends' adsCount={4} bannerUrl="gamer-2.png" />
-        <GameBanner title='Dota 2' adsCount={4} bannerUrl="gamer-3.png" />
-        <GameBanner title='CS:Go' adsCount={4} bannerUrl="gamer-4.png" />
-        <GameBanner title='APEX' adsCount={4} bannerUrl="gamer-5.png" />
-        <GameBanner title='Fortnite' adsCount={4} bannerUrl="gamer-6.png" />
+        {games.map(game => {
+          return (
+            <GameBanner
+              key={game.id}
+              title={game.title}
+              adsCount={game._count.ads}
+              bannerUrl={game.bannerUrl}
+            />
+          )
+        })}
       </div>
 
       <CreateAdBanner />
