1b931313ecb6a102ccb73fef9f9fef2633ef1cc3
Maycon Queiroz <fmaycon@gmail.com>
2022-09-17 18:05:30 -0300

    feat(components): add CreateAdBanner and GameBanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { MagnifyingGlassPlus } from 'phosphor-react'
+
 import './styles/main.css';
 import LogoImage from './assets/logo-nlw-esports.svg'
+import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
 
 function App() {
   return (
@@ -11,64 +13,15 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-10">
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="./gamer-1.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm">4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="./gamer-2.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm">4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="./gamer-3.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm">4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="./gamer-4.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm">4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="./gamer-5.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm">4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="./gamer-6.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm">4 anúncios</span>
-          </div>
-        </a>
+        <GameBanner title='Word WarCraff' adsCount={4} bannerUrl="gamer-1.png" />
+        <GameBanner title='League of Legends' adsCount={4} bannerUrl="gamer-2.png" />
+        <GameBanner title='Dota 2' adsCount={4} bannerUrl="gamer-3.png" />
+        <GameBanner title='CS:Go' adsCount={4} bannerUrl="gamer-4.png" />
+        <GameBanner title='APEX' adsCount={4} bannerUrl="gamer-5.png" />
+        <GameBanner title='Fortnite' adsCount={4} bannerUrl="gamer-6.png" />
       </div>
 
-      <div className="mt-8 pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
-        <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
-          <div>
-            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
-            <span className="text-zinc-400 block"> Public um anúncio para encontrar novos players</span>
-          </div>
-
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3">
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncios
-          </button>
-        </div>
-      </div>
+      <CreateAdBanner />
 
     </div>
   )
